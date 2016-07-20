01041ff24917bf8b1b2d806243f674d330ee3a30
BrindanJackson <brindanmj@gmail.com>
2016-07-19 22:14:49 -0400

    Some cleanup

--- hangmanMain.py
+++ hangmanMain.py
@@ -1,9 +1,13 @@
+# hangmanMain.py
+# Author: Brindan Jackson
+# Description: The game of hangman played through the console.
+
 from hangmanClasses import Game
 
 # Starting new hangman game
 newGame = Game()
 
-while newGame.roundFinished == 0:
+while newGame.gameFinished == 0:
     newGame.print_menu()
 
     menuChoice = input("Select a menu option: ")
@@ -12,11 +16,15 @@ while newGame.roundFinished == 0:
         print("Not a valid input. Please try again.")
 
     elif menuChoice == '1':
-        print("Play game")
+        '''Get word from file
+        Setup hidden word'''
+
+        while newGame.roundFinished == 0:
+            pass
 
     elif menuChoice == '2':
         print("Help Menu")
 
     elif menuChoice == '3':
         print("Game Ended")
-        newGame.set_round_finished(1)
+        newGame.set_game_finished(1)
